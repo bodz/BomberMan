a5c1a04a91828c5e759548de4e7df0488b74f904
Julien <julien.cavandoli@gmail.com>
2012-06-27 12:50:18 +0100

    commit : Cases

--- src/Models/Player.java
+++ src/Models/Player.java
@@ -6,4 +6,48 @@ import Utilitaires.*;
 
 public class Player {
 
+    private int posX;
+    private int posY;
+    private int health;
+    private int lifes;
+    
+    public Player(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+        this.health = 100;
+        this.lifes = 3;
+    }
+    
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+    
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+    
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    public void setLifes(int lifes) {
+        this.lifes = lifes;
+    }
+    
+    public int getPosX() {
+        return posX;
+    }
+    
+    public int getPosY() {
+        return posY;
+    }
+    
+    public int getHealth() {
+        return health;
+    }
+    
+    public int getLifes() {
+        return lifes;
+    }
+  
 }
